fa27447c74141902af7d41c12ad333e8127c1f2b
Huy Quang Nguyen <quanghuy1242@gmail.com>
2024-08-21 23:35:02 +0700

    All APIs but without testing, ready for UI integration

--- src/apis/images.ts
+++ src/apis/images.ts
@@ -26,6 +26,7 @@ const r2 = new AwsClient({
   accessKeyId: processEnv.R2_ACCESS_KEY_ID || "",
   secretAccessKey: processEnv.R2_SECRET_ACCESS_KEY || "",
 });
+const BASE_CF_IMAGE_URL = `https://${processEnv.BUCKET_NAME}.${processEnv.ACCOUNT_ID}.r2.cloudflarestorage.com`;
 
 const images = new Hono<HonoApp>().basePath("/images");
 
@@ -66,28 +67,24 @@ images.post(
         id: imageId,
         status: "PENDING",
         fullPath: `/${input.userId}/${now}/${imageId}/image.${ext}`,
-        previewPath: "",
+        previewPath: `/${input.userId}/${now}/${imageId}/image.webp`,
       },
     });
 
     // Request presigned url for uploading
-    const { ACCOUNT_ID, BUCKET_NAME }: Env = env(c as Context);
     const ttl = 5 * 60 * 60;
-    const signed = await r2.sign(
-      `https://${BUCKET_NAME}.${ACCOUNT_ID}.r2.cloudflarestorage.com${data.fullPath}`,
-      {
-        method: "PUT",
-        aws: { signQuery: true, allHeaders: true },
-        headers: {
-          "X-Amz-Expires": ttl.toString(),
-          "Content-Type": data.contentType,
-          "Content-Length": data.size.toString(),
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Allow-Headers": "Content-Type, Content-Length",
-        },
+    const signed = await r2.sign(`${BASE_CF_IMAGE_URL}${data.fullPath}`, {
+      method: "PUT",
+      aws: { signQuery: true, allHeaders: true },
+      headers: {
+        "X-Amz-Expires": ttl.toString(),
+        "Content-Type": data.contentType,
+        "Content-Length": data.size.toString(),
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers": "Content-Type, Content-Length",
       },
-    );
+    });
 
     return c.json(
       {
@@ -96,6 +93,120 @@ images.post(
       },
       201,
     );
+  },
+);
+
+images.post(
+  "/:id/validate",
+  authPrivate,
+  zValidator("param", z.object({ id: z.string().uuid() })),
+  async (c) => {
+    // Fetch the image db
+    const p = withPrisma(c);
+    const image = await p.image.findFirst({
+      where: {
+        id: c.req.valid("param").id,
+        ...withAuthQuery(c),
+      },
+    });
+    if (!image || image.status !== "PENDING") {
+      throw new DbConstraintException({ message: "Invalid params" });
+    }
+
+    const { IMAGE }: Env = env(c as Context);
+
+    // If the db record is there, request for the preview webp
+    const imageResponse = await r2.fetch(
+      `${BASE_CF_IMAGE_URL}${image.fullPath}`,
+      {
+        method: "GET",
+        cf: {
+          image: {
+            fit: "scale-down",
+            height: 200,
+            format: "webp",
+            quality: 50,
+          },
+        },
+      },
+    );
+
+    // If the image is still not there
+    if (!imageResponse.ok) {
+      // If there's something that holding users for uploading
+      // Then that image will become stale and need a new uploading
+      if (new Date().getSeconds() - image.created.getSeconds() >= 3600) {
+        await p.image.update({
+          where: { id: image.id },
+          data: { status: "INACTIVE" },
+        });
+        throw new DbConstraintException({
+          message: "Your creation has been expired, please upload a new one!",
+        });
+      }
+
+      // But if it's too soon, then just wait
+      return c.json(
+        { message: "The image is being processed, please wait!" },
+        200,
+      );
+    }
+
+    // If there is an image, then populate the preview
+    await IMAGE.put(image.previewPath, imageResponse.body);
+    await p.image.update({
+      where: { id: image.id },
+      data: { status: "ACTIVE" },
+    });
+    return c.json({ message: "Ok" });
+  },
+);
+
+images.get(
+  "/:id/:mode",
+  authPrivate,
+  zValidator(
+    "param",
+    z.object({ id: z.string().uuid(), mode: z.enum(["view", "preview"]) }),
+  ),
+  async (c) => {
+    const { id, mode } = c.req.valid("param");
+
+    // Fetch the image db
+    const p = withPrisma(c);
+    const image = await p.image.findFirst({
+      where: { id: id, ...withAuthQuery(c) },
+    });
+    if (!image || image.status !== "ACTIVE") {
+      throw new DbConstraintException({ message: "Not found" });
+    }
+
+    const path: { [key in typeof mode]: string } = {
+      view: image.fullPath,
+      preview: image.previewPath,
+    };
+    const imageKey = `${BASE_CF_IMAGE_URL}${path[mode]}`;
+
+    const { R2PU }: Env = env(c as Context);
+    let cacheKey = await R2PU.get(imageKey);
+
+    if (!cacheKey) {
+      const ttl = 5 * 60 * 60;
+      const signed = await r2.sign(imageKey, {
+        method: "GET",
+        aws: { signQuery: true, allHeaders: true },
+        headers: {
+          "X-Amz-Expires": ttl.toString(),
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Headers": "Content-Type, Content-Length",
+        },
+      });
+      await R2PU.put(imageKey, signed.url, { expirationTtl: ttl - 5 });
+      cacheKey = signed.url;
+    }
+
+    return c.redirect(cacheKey);
   },
 );
 
